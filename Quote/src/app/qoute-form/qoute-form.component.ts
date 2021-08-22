b331bd930981f800e2ba6d65a2d201e28f3343d8
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-08-22 14:51:35 +0300

    add the output method and import its variables

--- Quote/src/app/qoute-form/qoute-form.component.ts
+++ Quote/src/app/qoute-form/qoute-form.component.ts
@@ -7,9 +7,7 @@ import { Quote } from '../quote';
   styleUrls: ['./qoute-form.component.scss']
 })
 export class QouteFormComponent implements OnInit {
-
-  
-  newQuote = new Quote(0,"","",0,0,"",new Date());
+ newQuote = new Quote(0,"","",0,0,"",new Date());
   @Output() addQuote = new EventEmitter<Quote>();
   submitQuote(){
     this.addQuote.emit(this.newQuote);
